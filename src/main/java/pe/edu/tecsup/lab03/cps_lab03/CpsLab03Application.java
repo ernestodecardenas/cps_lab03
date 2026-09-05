4b988c4cb322ad6cd105d10f28f8c3c7d5ec99d0
Ernesto <ernesto.decardenas@ecsup.edu.pe>
2026-09-05 11:13:42 -0500

    first commit

---
+++ src/main/java/pe/edu/tecsup/lab03/cps_lab03/CpsLab03Application.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.lab03.cps_lab03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpsLab03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CpsLab03Application.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/tecsup/lab03/cps_lab03/CpsLab03ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.lab03.cps_lab03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpsLab03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
